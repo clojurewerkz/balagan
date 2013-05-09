0cf394654b8fac1c42589ffb5adc760b98f552c1
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-05-09 23:38:29 +0200

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ifesdjeen/balagan "0.1.0"
+(defproject com.ifesdjeen/balagan "0.1.1-SHAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
