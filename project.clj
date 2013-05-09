c47159784c63a5e37adde9ba2e0212fe876a3540
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-05-09 23:35:16 +0200

    Release v0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ifesdjeen/balagan "0.1.0-SNAPSHOT"
+(defproject com.ifesdjeen/balagan "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
