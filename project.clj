5b43f994e137898d02f5c9ddd6ac594ccce5c7ab
Alex Petrov <alexp@coffeenco.de>
2013-08-30 11:18:12 +0200

    Release 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/balagan "0.1.1-SHAPSHOT"
+(defproject clojurewerkz/balagan "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
