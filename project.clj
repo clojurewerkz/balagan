333f51cd160fb809d5f69848d537a9822e957af9
Alex Petrov <alexp@coffeenco.de>
2013-08-30 11:19:04 +0200

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/balagan "0.2.0"
+(defproject clojurewerkz/balagan "0.3.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
