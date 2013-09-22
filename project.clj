5cde6bc0165e564e2928d43af4dafffe74db063e
Alex Petrov <alexp@coffeenco.de>
2013-09-22 21:20:58 +0200

    Release 0.3.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojurewerkz/balagan "0.3.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojurewerkz/balagan "0.3.0"
+  :description "A tiny library for data structure transformation and querying"
+  :url "http://github.com/clojurewerkz/balagan"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]])
