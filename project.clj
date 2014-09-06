c4f432a997457dd90db224fe1ec539f09b3b5935
Michael Klishin <michael@clojurewerkz.org>
2014-09-06 18:44:48 +0400

    Test against Clojure 1.7

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
 
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]]
                    :plugins [[codox "0.8.10"]
@@ -37,7 +38,7 @@
                                                     :optimizations :advanced}}]}
                    :codox {:sources ["src/cljx"]
                            :output-dir "doc/api"}}}
-  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master"]}
+  :aliases  {"all" ["with-profile" "+dev:+1.4:+1.5:+master:+1.7"]}
   :jar-exclusions [#"\.cljx"]
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
