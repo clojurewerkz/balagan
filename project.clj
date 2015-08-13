aaa75a45d97e6df678b5531c15aa360698f442d3
Michael Klishin <michael@clojurewerkz.org>
2015-08-13 13:39:26 +0300

    1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/balagan "1.1.0-SNAPSHOT"
+(defproject clojurewerkz/balagan "1.0.2"
   :description "A tiny library for data structure transformation and querying"
   :url "http://github.com/clojurewerkz/balagan"
   :license {:name "Eclipse Public License"
